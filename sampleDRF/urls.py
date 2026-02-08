6241cfde9d6c7540be569b943ef20ae1c9272528
Karami <v.karami@rahpoo.ir>
2026-02-08 16:16:11 +0330

    created api router and viewset.

--- sampleDRF/urls.py
+++ sampleDRF/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from sampleDRF.views import hello_geeks
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('geek/', hello_geeks)
+    path('geek/', hello_geeks),
+    path('api/', include('app.urls')),
 ]
 
 
